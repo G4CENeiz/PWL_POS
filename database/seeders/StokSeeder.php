41d02d2cc7c002a17a7efe3e5763b847fbbd41c2
Baihaqi <baihaqilearning+github@gmail.com>
2024-03-15 13:49:40 +0700

    feat: practicum-3

--- database/seeders/StokSeeder.php
+++ database/seeders/StokSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class StokSeeder extends Seeder
 {
@@ -16,10 +17,74 @@ class StokSeeder extends Seeder
             [
                 'stok_id' => 1,
                 'barang_id' => 1,
-                'user_id' => 1,
-                'stok_tanggal' => 1,
-                'stok_jumlah' => 1,
+                'user_id' => 3,
+                'stok_tanggal' => 20240315,
+                'stok_jumlah' => 20,
+            ],
+            [
+                'stok_id' => 2,
+                'barang_id' => 2,
+                'user_id' => 3,
+                'stok_tanggal' => 20240315,
+                'stok_jumlah' => 20,
+            ],
+            [
+                'stok_id' => 3,
+                'barang_id' => 3,
+                'user_id' => 3,
+                'stok_tanggal' => 20240315,
+                'stok_jumlah' => 20,
+            ],
+            [
+                'stok_id' => 4,
+                'barang_id' => 4,
+                'user_id' => 3,
+                'stok_tanggal' => 20240315,
+                'stok_jumlah' => 20,
+            ],
+            [
+                'stok_id' => 5,
+                'barang_id' => 5,
+                'user_id' => 3,
+                'stok_tanggal' => 20240315,
+                'stok_jumlah' => 20,
+            ],
+            [
+                'stok_id' => 6,
+                'barang_id' => 6,
+                'user_id' => 3,
+                'stok_tanggal' => 20240315,
+                'stok_jumlah' => 20,
+            ],
+            [
+                'stok_id' => 7,
+                'barang_id' => 7,
+                'user_id' => 3,
+                'stok_tanggal' => 20240315,
+                'stok_jumlah' => 20,
+            ],
+            [
+                'stok_id' => 8,
+                'barang_id' => 8,
+                'user_id' => 3,
+                'stok_tanggal' => 20240315,
+                'stok_jumlah' => 20,
+            ],
+            [
+                'stok_id' => 9,
+                'barang_id' => 9,
+                'user_id' => 3,
+                'stok_tanggal' => 20240315,
+                'stok_jumlah' => 20,
+            ],
+            [
+                'stok_id' => 10,
+                'barang_id' => 10,
+                'user_id' => 3,
+                'stok_tanggal' => 20240315,
+                'stok_jumlah' => 20,
             ],
         ];
+        DB::table('t_stok')->insert($data);
     }
 }
