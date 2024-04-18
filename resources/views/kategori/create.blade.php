daf75aaf3f280770f5daaa99ebe91e6b971883d8
Baihaqi <baihaqilearning+github@gmail.com>
2024-04-19 01:08:58 +0700

    feat(JS5): practicum steps

---
+++ resources/views/kategori/create.blade.php
@@ -0,0 +1,32 @@
+@extends('layout.app')
+{{-- Customize layout sections --}}
+@section('subtitle', 'Kategori')
+@section('content_header_title', 'Kategori')
+@section('content_header_subtitle', 'Create')
+{{-- Content Body: main page content --}}
+@section('content')
+    <div class="container">
+        <div class="card card-primary">
+            <div class="card-header">
+                <h3 class="card-title">Buat kategori baru</h3>
+            </div>
+
+            <form action="../kategori" method="post">
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="kodeKategori">Kode Kategori</label>
+                        <input type="text" name="kodeKategori" id="kodeKategori" class="form-control" placeholder="Masukkan Kode Kategori">
+                    </div>
+                    <div class="form-group">
+                        <label for="namaKategori">Nama Kategori</label>
+                        <input type="text" name="namaKategori" id="namaKategori" class="form-control" placeholder="Masukkan Nama Kategori">
+                    </div>
+                </div>
+
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
