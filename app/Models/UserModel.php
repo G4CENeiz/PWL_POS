4d2b0e5303f7d727ca981a4d0ad8b6289023a6c5
Baihaqi <baihaqilearning+github@gmail.com>
2024-05-31 10:52:07 +0700

    feat(JS10): practicum 1 - RESTful API Register

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -7,11 +7,22 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    // use HasApiTokens, HasFactory, Notifiable;
 
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return[];
+    }
+    
     protected $table = 'm_user';
     public $timestamps = false;
     protected $primaryKey = 'user_id';
