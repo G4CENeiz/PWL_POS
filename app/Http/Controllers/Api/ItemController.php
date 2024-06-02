3dc7c3dc1cbe2d65cf59d1db9d5c216a1f4876dc
Baihaqi <baihaqilearning+github@gmail.com>
2024-06-02 19:13:42 +0700

    feat(JS11): assignment

--- app/Http/Controllers/Api/ItemController.php
+++ app/Http/Controllers/Api/ItemController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\BarangModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ItemController extends Controller
 {
@@ -13,8 +14,38 @@ class ItemController extends Controller
     }
 
     public function store(Request $request) {
-        $item = BarangModel::create($request->all());
-        return response()->json($item, 201);
+        $validator = Validator::make($request->all(), [
+            'kategori_id'   => 'required',
+            'barang_kode'   => 'required',
+            'barang_nama'   => 'required',
+            'harga_beli'    => 'required',
+            'harga_jual'    => 'required',
+            'image'         => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $item = BarangModel::create([
+            'kategori_id'   => $request->kategori_id,
+            'barang_kode'   => $request->barang_kode,
+            'barang_nama'   => $request->barang_nama,
+            'harga_beli'    => $request->harga_beli,
+            'harga_jual'    => $request->harga_jual,
+            'image'  => $request->image->hashName(),
+        ]);
+
+        if ($item) {
+            return response()->json([
+                'success' => true,
+                'item' => $item,
+            ], 201);
+        }
+
+        return response()->json([
+            'success' => false,
+        ], 409);
     }
 
     public function show(BarangModel $item) {
