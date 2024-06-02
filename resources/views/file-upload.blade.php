00d5095f5870a1f1a945f62253dd91d0ff6db5cc
Baihaqi <baihaqilearning+github@gmail.com>
2024-06-02 23:15:43 +0700

    feat(JS11): preparation

---
+++ resources/views/file-upload.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>File Upload</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container mt-3">
+        <h2>File Upload</h2>
+        <form action="{{ url('/file-upload') }}" method="post" enctype="multipart/form-data">
+            @csrf
+
+            <div class="mb-3">
+                <label for="document" class="form-label">Gambar Profile</label>
+                <input type="file" name="document" id="document" class="form-control">
+                @error('document')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <button type="submit" class="btn btn-primary my-2">Upload</button>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+</html>
