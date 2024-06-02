09a5c0db33463d8b320ccfce51645148ac2eb19c
Baihaqi <baihaqilearning+github@gmail.com>
2024-06-02 23:45:52 +0700

    feat(JS12): moving file upload

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -14,23 +14,11 @@ class FileUploadController extends Controller
         // return 'Hello World';
         // dump($request->document);
         $request->validate([
-            'document' => 'required|file|image|max:500',
+            'document' => 'required|file|image|max:5000',
         ]);
-        if ($request->hasFile('document')) {
-            echo 'path(): ' . $request->document->path();
-            echo '<br>';
-            echo 'extension(): ' . $request->document->extension();
-            echo '<br>';
-            echo 'getClientOriginalExtension(): ' . $request->document->getClientOriginalExtension();
-            echo '<br>';
-            echo 'getMimeType(): ' . $request->document->getMimeType();
-            echo '<br>';
-            echo 'getClientOriginalName(): ' . $request->document->getClientOriginalName();
-            echo '<br>';
-            echo 'getSize(): ' . $request->document->getSize();
-            echo '<br>';
-        } else {
-            echo 'no document has been uploaded';
-        }
+        $fileExt = $request->document->getClientOriginalName();
+        $fileName = 'web-' . time() . '.' . $fileExt;
+        $path = $request->document->storeAs('uploads', $fileName);
+        echo 'upload process successful, file is located at: ' . $path;
     }
 }
